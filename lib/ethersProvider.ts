caa934225d983e9d62f4a1cccaa0d7b98b4f9ce0
LY Cheng <yhsiang@token.im>
2022-06-09 11:02:01 +0800

    fix window

--- lib/ethersProvider.ts
+++ lib/ethersProvider.ts
@@ -1,6 +1,10 @@
 import { ethers } from "ethers"
 
-const url = `${window.location.host}/api/json_rpc`
+let host = "http://localhost:3000"
+if (typeof window !== "undefined") {
+    host = window.location.host
+}
+const url = `${host}/api/json_rpc`
 
 const provider = new ethers.providers.JsonRpcProvider(url)
 
